cca8cdfe853c6246a0b7193a64b5c72ebfe274ca
Antony Chazapis <chazapis@gmail.com>
2019-02-14 07:48:13 +0200

    Initial Commit

---
+++ Estrella/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Estrella
+//
+//  Created by Antony Chazapis on 14/02/2019.
+//  Copyright © 2019 Koomasi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Estrella/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Estrella
+//
+//  Created by Antony Chazapis on 14/02/2019.
+//  Copyright © 2019 Koomasi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
